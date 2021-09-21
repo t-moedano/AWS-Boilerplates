67276ca7850d9a96f9149a64fd32440ceff414be
Thauany Moedano <thauany@zappts.com>
2021-09-20 21:29:27 -0300

    add boilerplate for backend study

---
+++ serverless-backend/backend-lambda-dynamo/README.md
@@ -0,0 +1,17 @@
+## Olá, bem vindo ao repositório de aprendizado! 👋
+
+
+## Install
+
+```sh
+yarn install 
+```
+
+## Autora
+
+👤 **Thauany Moedano**
+
+
+## Mostre seu suporte :D
+
+Dê uma ⭐️ se você aprendeu com esse projeto!
---
+++ serverless-backend/backend-lambda-dynamo/app/src/handler/methods.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable arrow-body-style */
+/* eslint-disable import/prefer-default-export */
+import { GradeEvent } from '../interfaces/GradeEvent';
+
+export const calculateGrades = async (event: GradeEvent): Promise<string> => {
+  // insira seu código aqui
+  return 'ok';
+};
